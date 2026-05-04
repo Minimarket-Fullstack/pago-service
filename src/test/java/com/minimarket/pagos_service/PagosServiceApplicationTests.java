541e989a641797c7f88b13ebb0c8afeee4a061ab
neurott <neurott@protonmail.com>
2026-05-04 12:11:38 -0400

    primer push

---
+++ src/test/java/com/minimarket/pagos_service/PagosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minimarket.pagos_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
