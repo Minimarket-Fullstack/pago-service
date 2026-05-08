bfa7873e662926b66133b1c93bee08fe93eba645
neurott <neurott@protonmail.com>
2026-05-08 17:42:44 -0400

    a

--- src/main/java/com/minimarket/pagos_service/service/PagoService.java
+++ src/main/java/com/minimarket/pagos_service/service/PagoService.java
@@ -1,4 +1,12 @@
 package com.minimarket.pagos_service.service;
 
+import com.minimarket.pagos_service.repository.PagoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PagoService {
+
+    private final PagoRepository pagoRepository;
 }
